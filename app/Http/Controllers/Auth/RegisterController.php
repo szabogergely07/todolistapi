de346deb6aee6cd6dbc48674fa740c0d6ce3e5bd
szabogergely07 <szabogergely07@gmail.com>
2018-09-26 14:19:06 +0200

    Update register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -37,7 +37,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        //$this->middleware('guest');
     }
 
     /**
@@ -48,11 +48,11 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        // return Validator::make($data, [
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|string|email|max:255|unique:users',
-        //     'password' => 'required|string|min:6|confirmed',
-        // ]);
+        return Validator::make($data, [
+            0 => 'required|string|max:255',
+            1 => 'required|string|email|max:255|unique:users',
+            2 => 'required|string|min:6',
+        ]);
     }
 
     /**
